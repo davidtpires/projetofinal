f31ab9aedee39577e439e6d92fc9603bbc9ccb0a
davidtpires <davidt.info@gmail.com>
2020-06-21 23:57:34 -0300

    Aulas até 170

---
+++ core/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Marca, Veiculo, Pessoa, Parametros, MovRotativo
+
+class MovRotativoAdmin(admin.ModelAdmin):
+	list_display = (
+		'checkin', 'checkout', 'valor_hora', 'veiculo', 'pago', 'total', 'horas_total')
+
+admin.site.register(Marca)
+admin.site.register(Veiculo)
+admin.site.register(Pessoa)
+admin.site.register(Parametros)
+admin.site.register(MovRotativo, MovRotativoAdmin)
+# Register your models here.
